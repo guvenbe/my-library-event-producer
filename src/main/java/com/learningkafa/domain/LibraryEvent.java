50e64cdae043a2da1a0174a14270411c997fa715
Bora Guven <guvenbe@gmail.com>
2021-10-04 06:35:10 +0000

    add lombok to LibraryEvent.java

--- src/main/java/com/learningkafa/domain/LibraryEvent.java
+++ src/main/java/com/learningkafa/domain/LibraryEvent.java
@@ -1,4 +1,14 @@
 package com.learningkafa.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class LibraryEvent {
+    private Integer libraryEvent;
 }
